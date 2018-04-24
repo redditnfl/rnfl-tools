45f0362375412cb68c7ffd164c3aa0682906a8bf
Jonas Häggqvist <rasher@rasher.dk>
2018-04-24 23:18:59 +0200

    Refresh after filling rounds select

--- draftcardposter/static/draftcardposter/draftcardposter.js
+++ draftcardposter/static/draftcardposter/draftcardposter.js
@@ -4,19 +4,17 @@ $(document).ready(function() {
     $.getJSON('picks', function(data) {
         picks = data.picks[data.current_year];
         for (var i=1; i <= picks.length; i++) {
-            console.log("Add round " + i);
             $("#round").append($('<option>', { value: i, text: i }));
         }
     }).done(function() {
+        $("#round").selectpicker('refresh');
         $("#round").on("changed.bs.select",
             function(e, clickedIndex, newValue, oldValue) {
                 console.log(this.value, clickedIndex, newValue, oldValue);
-                console.log(picks[this.value - 1]);
                 $("#pick").find('option').remove();
                 var round = picks[this.value - 1];
                 for (var i=0; i < round.length; i++) {
                     var pick = round[i];
-                    console.log("Add pick " + pick[0]);
                     var text = pick[0];
                     if (pick[1] == "c") text += " (comp)";
                     $("#pick").append($('<option>', { value: pick[0], text: text}));
