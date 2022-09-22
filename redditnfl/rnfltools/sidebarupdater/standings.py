083f86a34b955256becf7ae2d3f9b95d075340c8
Jonas Häggqvist <rasher@rasher.dk>
2022-09-22 21:51:45 +0200

    Fix standings

--- redditnfl/rnfltools/sidebarupdater/standings.py
+++ redditnfl/rnfltools/sidebarupdater/standings.py
@@ -4,42 +4,41 @@ from redditnfl.nfltools.nflteams import get_team
 
 from sgqlc.operation import Operation
 
-
-def division(div):
-    return div[0:3], div[4:].title()
+from pprint import pprint
 
 
 def division_standings(teams):
     standings = {}
     for team, record in teams:
-        div = division(team.division)
+        div = (team.conference_abbr, team.division_full_name.split(' ')[1])
         if div not in standings:
             standings[div] = []
         standings[div].append((team, record))
 
     for div in standings:
-        standings[div].sort(key=lambda t: (1 - t[1].overall_pct, t[1].division_rank, t[0].full_name))
+        standings[div].sort(key=lambda t: t[1].division.rank)
+
     return standings
 
 
 def fmt_record(standings):
-    ret = '{s.overall_win}-{s.overall_loss}'.format(s=standings)
-    if standings.overall_tie:
-        ret += '-{s.overall_tie}'.format(s=standings)
+    ret = '{s.overall.wins}-{s.overall.losses}'.format(s=standings)
+    if standings.overall.ties:
+        ret += '-{s.overall.ties}'.format(s=standings)
     return ret
 
 
 def fmt_playoffs(standings):
     symbols = [
-        ("clinch_playoff", "x"),
-        ("clinch_division", "y"),
-        ("clinch_wc", "z"),
-        ("clinch_division_and_homefield", "*"),
-        ("eliminated_from_postseason", "†"),
+        ("playoff", "x"),
+        ("division", "y"),
+        ("wild_card", "z"),
+        ("home_field", "*"),
+        ("eliminated", "†"),
     ]
     ret = ""
     for attr, s in symbols:
-        if getattr(standings, attr, False):
+        if getattr(standings.clinched, attr, False):
             ret += s
     return "^" + ret if ret else ret
 
@@ -47,40 +46,9 @@ def fmt_playoffs(standings):
 def run(reddit_session, **config):
     nfl = NFL(ua='nfl scores sidebar')
     ret = config['header']
-    week = nfl.schedule.current_week()
+    records = nfl.standings.current()
 
-    op = Operation(shield.Viewer)
-    standings = op.viewer.teams_group.standings(first=1, week_season_value=week.season_value, week_week_value=week.week_value, week_season_type=week.season_type, order_by=WeekOrderBy.week__weekOrder,
-                                                order_by_direction=OrderByDirection.DESC)
-    standing = standings.edges.node
-    record = standing.team_records
-    record.team_id()
-    record.overall_win()
-    record.overall_loss()
-    record.overall_tie()
-    record.overall_pct()
-    record.division_rank()
-    record.clinch_playoff()
-    record.clinch_division()
-    record.clinch_wc()
-    record.clinch_division_and_homefield()
-    record.eliminated_from_postseason()
-
-    res = nfl.query(op)
-    if len(res.viewer.teams_group.standings.edges) == 0:
-        return ''
-    records = res.viewer.teams_group.standings.edges[0].node.team_records
-    records = {r.team_id: r for r in records}
-
-    def team_fun(t: shield.Team):
-        t.id()
-        t.abbreviation()
-        t.division()
-        t.full_name()
-
-    teams = nfl.team.by_ids(records.keys(), select_fun=team_fun)
-    teams = [(t, records[t.id]) for t in teams]
-    standings = division_standings(teams)
+    standings = division_standings(records)
 
     for conference in ('AFC', 'NFC'):
         for left_division, right_division in (('North', 'South'), ('East', 'West')):
